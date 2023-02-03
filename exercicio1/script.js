35a2ae74d6157457c4fe1c8132b9b3e37614d391
Babuhdiniz <babuhdiniz@gmail.com>
2023-02-02 21:45:27 -0300

    modificacoes operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let par = Number(prompt("Insira um número par:"))
+console.log(`O número inserido foi: ${par}`)
+
+const resto = par % 2
+console.log(resto)
+
+
+// Os números pares sempre retornam resto zero. A variável é par e criei uma operação que divide essa operação sempre por 2 e consequentemente retorna o 0.
+// Caso seja inserido um número impar, irá ''sobrar'' e retornar 1
